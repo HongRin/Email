5f7976df4d52beaaad3e6aff7866d6d685d5c0c2
HongRin <72390797+HongRin@users.noreply.github.com>
2021-06-14 21:24:01 +0900

    StringDivision
    @ 기준으로 입력 받은 이메일을 나눔

--- Source/Email/Basic/EmailWnd/EmailWnd.cpp
+++ Source/Email/Basic/EmailWnd/EmailWnd.cpp
@@ -14,5 +14,20 @@ void UEmailWnd::NativeConstruct()
 
 void UEmailWnd::OnEmailCommitted(const FText& Text, ETextCommit::Type CommitMethod)
 {
+	// 엔터키를 눌렀다면
+	if (CommitMethod == ETextCommit::OnEnter)
+	{
+		// 입력한 email 을 FString 으로 담을 변수
+		FString emailStr = Text.ToString();
 
+		// email을 @ 기준으로 나눠 저장할 배열
+		TArray<FString> emailArg;
+
+		// @ 기준으로 문자열을 나눔
+		emailStr.ParseIntoArray(emailArg, TEXT("@"));
+
+		// 출력
+		Text_Output->SetText(FText::FromString(
+			TEXT("LocaleName[") + emailArg[0] + TEXT("]::UserName[") + emailArg[1] + TEXT("]")));
+	}
 }
